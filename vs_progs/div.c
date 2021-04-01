a63f4c5c30b88ff7d963177a9c218db8e2f0aad1
udaykiran640 <udaykiran640@gmail.com>
2021-04-01 11:42:37 +0530

    dated010421

---
+++ vs_progs/div.c
@@ -0,0 +1,6 @@
+#include"fun.h"
+
+int div(int num1,int num2)
+{
+    return num1/num2;
+}
---
+++ vs_progs/mains.c
@@ -0,0 +1,10 @@
+#include"stdio.h"
+#include"fun.h"
+
+int main()
+{
+    int a,b;
+    a=add(3,2);
+    b=div(4,2);
+    return 0;
+}
